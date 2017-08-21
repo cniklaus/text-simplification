353dfa5cc057f322a856c595b3fe6aa0d08a2989
Sam Davidson <25775147+ssdavidson@users.noreply.github.com>
2017-08-21 13:33:27 -0700

    Add files via upload
    Update split_data.py to split normal and simple data in one script

--- split_data.py
+++ split_data.py
@@ -8,13 +8,14 @@ list3 = list1[2000:3999]
 
 
 line_count = 1
-filename = "normal.aligned"
+filename1 = "normal.aligned"
+filename2 = "simple.aligned"
 
-f1 = io.open("/home/khgkim/torch/OpenNMT/data2/data3" + '/test.txt', mode='a+', encoding='utf-8')
-f2 = io.open("/home/khgkim/torch/OpenNMT/data2/data3" + '/val.txt', mode='a+', encoding='utf-8')
-f3 = io.open("/home/khgkim/torch/OpenNMT/data2/data3" + '/train.txt', mode='a+', encoding='utf-8')
+f1 = io.open("/home/khgkim/torch/OpenNMT/data2/data3" + '/test_normal.txt', mode='a+', encoding='utf-8')
+f2 = io.open("/home/khgkim/torch/OpenNMT/data2/data3" + '/val_normal.txt', mode='a+', encoding='utf-8')
+f3 = io.open("/home/khgkim/torch/OpenNMT/data2/data3" + '/train_normal.txt', mode='a+', encoding='utf-8')
 
-with io.open("/home/khgkim/torch/OpenNMT/data2/data3" + '/' + filename, encoding='utf-8') as s:
+with io.open("/home/khgkim/torch/OpenNMT/data2/data3" + '/' + filename1, encoding='utf-8') as s:
   for line in s:
     stripped = line.split('\t')[2]
     if line_count in list2:
@@ -24,3 +25,21 @@ with io.open("/home/khgkim/torch/OpenNMT/data2/data3" + '/' + filename, encoding
     else:
       f3.write(stripped + '\n')
     line_count += 1
+
+line_count = 1
+
+f1 = io.open("/home/khgkim/torch/OpenNMT/data2/data3" + '/test_simple.txt', mode='a+', encoding='utf-8')
+f2 = io.open("/home/khgkim/torch/OpenNMT/data2/data3" + '/val_simple.txt', mode='a+', encoding='utf-8')
+f3 = io.open("/home/khgkim/torch/OpenNMT/data2/data3" + '/train_simple.txt', mode='a+', encoding='utf-8')
+
+with io.open("/home/khgkim/torch/OpenNMT/data2/data3" + '/' + filename2, encoding='utf-8') as s:
+  for line in s:
+    stripped = line.split('\t')[2]
+    if line_count in list2:
+      f1.write(stripped + '\n')
+    elif line_count in list3:
+      f2.write(stripped + '\n')
+    else:
+      f3.write(stripped + '\n')
+    line_count += 1
+
